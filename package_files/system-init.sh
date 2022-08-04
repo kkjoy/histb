ab002e6086ff74567dc619b1568f54c1bfe63c37
xjm1285 <xjm1285@gmail.com>
2022-08-04 12:01:44 +0000

    remover manual insert fstab for swap

--- package_files/system-init.sh
+++ package_files/system-init.sh
@@ -27,7 +27,6 @@ then
 	chmod 600 /swapfile
 	mkswap /swapfile
 	swapon /swapfile
-	echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
 } &
 fi
 grep -q '/swapfile' /etc/fstab || echo "/swapfile swap swap defaults,nofail 0 0" >> /etc/fstab
